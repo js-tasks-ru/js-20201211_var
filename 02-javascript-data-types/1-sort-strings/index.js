226963c9a35b03479606ddb6ae4197d81c511892
navff <var@33kita.ru>
2020-12-16 06:11:00 +0300

    02-01: Sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,24 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const result = arr.slice(0);
 
+  return result.sort(function(str1, str2) {
+    if ((isEnglish(str1)) && (!isEnglish(str2))) {
+      return param === 'asc' ? 1 : -1;
+    }
+    if ((isEnglish(str2)) && (!isEnglish(str1))) {
+      return param === 'asc' ? -1 : 1;
+    }
+
+    if (param === 'asc') {
+      return str1.localeCompare(str2, ['ru', 'ru-co-phonebk'], { caseFirst: 'upper' });
+    }
+    return str2.localeCompare(str1, undefined, { caseFirst: 'upper' });
+  }).slice(0);
+}
+
+function isEnglish(str) {
+  const english = /^[A-Za-z]*$/;
+  return english.test(str[0]);
 }
