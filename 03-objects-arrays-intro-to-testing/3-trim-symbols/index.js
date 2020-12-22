3c92ce552a3513d56a9b0acd661339dfd59b74ac
navff <var@33kita.ru>
2020-12-22 03:36:48 +0300

    03-03: trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
+  if (size === 0 || string === '') {
+    return '';
+  }
 
+  const symbols = string.split('');
+  let repeatCount = 0;
+  return symbols.reduce(((result, currentValue) => {
+    if (result.slice(-1) === currentValue) {
+      repeatCount += 1;
+      if (repeatCount >= size) {
+        repeatCount = 0;
+        return result;
+      }
+    }
+    result += currentValue;
+    return result;
+  }));
 }
