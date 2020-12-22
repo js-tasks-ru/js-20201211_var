311cfa644325af281ffda850bea4792259a48fe2
navff <var@33kita.ru>
2020-12-22 03:42:08 +0300

    03-04: unique

---
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -0,0 +1,9 @@
+/**
+ * uniq - returns array of uniq values:
+ * @param {*[]} arr - the array of primitive values
+ * @returns {*[]} - the new array with uniq values
+ */
+export function uniq(arr) {
+  const tempSet = new Set(arr);
+  return Array.from(tempSet);
+}
